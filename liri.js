1ebed8740ceda50f1d46b86d84aa362d2669a929
DevonMM <Devon.m.moran@gmail.com>
2019-01-18 19:25:58 -0500

    its working

--- liri.js
+++ liri.js
@@ -1,12 +1,15 @@
 require("dotenv").config();
 
 var keys = require("./keys.js");
-var spotify = new Spotify(keys.spotify);
+
 var Spotify = require("node-spotify-api");
 var request = require("request");
+
 var fs = require("fs");
 var moment = require("moment");
 moment().format();
+
+var spotify = new Spotify(keys.spotify);
 
 var command = process.argv[2];
 var input = process.argv[3];
@@ -22,8 +25,8 @@ function bandsInArea(bandQuery) {
     if (!error && response.statusCode === 200) {
       var concertData = JSON.parse(body);
 
-      var concertDateTime = concertData[0].datetime
-      var momentDateTime = moment().format("L");
+      var concertDate = concertData[0].datetime
+      var momentDate = moment().format("L");
 
       console.log("------------------------------");
 
@@ -35,83 +38,12 @@ function bandsInArea(bandQuery) {
         "," +
         concertData[0].venue.country +
         "\nDate of the Event: " +
-        momentDateTime +
+        momentDate +
         "\n--------------------------"
       );
     }
   });
 }
-
-function findMovie(movieQUery) {
-  if (movieQUery === undefined || null) {
-    movieQUery = "Mr.Nobody";
-  }
-  var queryURL =
-    "http://www.omdbapi.com/?t=" + movieQuery + "&y=&plot=short&apikey=trilogy";
-
-  console.log(queryURL);
-
-  request(queryURL, function (error, response, body) {
-    if (!error && response.statusCode === 200) {
-      var movieData = JSON.parse(body);
-      console.log("----------------------------------");
-      console.log(
-        "Movie Title: " +
-        movieData.Title +
-        "\nYear: " +
-        movieData.released +
-        "\nIMDB Rating: " +
-        movieData.imdb.Rating +
-        "\n Rotten Tomatoes Rating: " +
-        movieData.Ratings[1].Value +
-        "\nCountry: " +
-        movieData.Country +
-        "\nLanguage: " +
-        movieData.Language +
-        "\nPlot: " +
-        +movieData.Plot +
-        "\nActors: " +
-        movieData.Actors +
-        "\n-----------------------------------"
-      );
-    }
-  });
-}
-findMovie();
-
-var askIt = function (commands, otherData) {
-  switch (commands) {
-    case "concert-this":
-      bandsInArea(otherData);
-      break;
-    case "movie-this":
-      findMovie(otherData);
-      break;
-    case "spotify-this":
-      spotifySong(otherData);
-      break;
-    case "do-what-it-says":
-      doWhatItSays();
-      break;
-    default:
-      console.log("Invalid. Please try again");
-  }
-};
-
-var doWhatItSays = function () {
-  fs.readFile("random.txt", "utf8", function (error, data) {
-    if (error) throw error;
-    var randomText = data.split(",");
-
-    if (randomText.length == 2) {
-      askIt(randomText[0], randomText[1]);
-    } else if (randomText.length == 1) {
-      askIt(randomText[0]);
-    }
-  });
-};
-
-askIt(command, input);
 
 function spotifySong(songSearch) {
 
@@ -136,3 +68,74 @@ function spotifySong(songSearch) {
       }
     })
 }
+
+function findMovie(movieQuery) {
+  if (movieQuery === undefined || null) {
+    movieQuery = "Mr.Nobody";
+  }
+  var queryURL =
+    "http://www.omdbapi.com/?t=" + movieQuery + "&y=&plot=short&apikey=trilogy";
+
+  console.log(queryURL);
+
+  request(queryURL, function (error, response, body) {
+    if (!error && response.statusCode === 200) {
+      var movieData = JSON.parse(body);
+      console.log("----------------------------------");
+      console.log(
+        "Movie Title: " +
+        movieData.Title +
+        "\nYear: " +
+        movieData.released +
+        "\nIMDB Rating: " +
+        movieData.imdbRating +
+        "\n Rotten Tomatoes Rating: " +
+        movieData.Ratings[1].Value +
+        "\nCountry: " +
+        movieData.Country +
+        "\nLanguage: " +
+        movieData.Language +
+        "\nPlot: " +
+        +movieData.Plot +
+        "\nActors: " +
+        movieData.Actors +
+        "\n-----------------------------------"
+      );
+    }
+  });
+}
+
+
+var askIt = function (commands, otherData) {
+  switch (commands) {
+    case "concert-this":
+      bandsInArea(otherData);
+      break;
+    case "movie-this":
+      findMovie(otherData);
+      break;
+    case "spotify-this":
+      spotifySong(otherData);
+      break;
+    case "do-what-it-says":
+      doWhatItSays();
+      break;
+    default:
+      console.log("Invalid. Please try again");
+  }
+};
+
+var doWhatItSays = function () {
+  fs.readFile("random.txt", "utf8", function (error, data) {
+    if (error) throw error;
+    var random = data.split(",");
+
+    if (random.length == 2) {
+      askIt(random[0], random[1]);
+    } else if (random.length == 1) {
+      askIt(random[0]);
+    }
+  });
+};
+
+askIt(command, input);
